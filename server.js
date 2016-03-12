90091166ba15016e73ebcb205db401647eb68869
rachelsmithcode <rachelcodesmith@outlook.com>
2016-03-12 15:47:35 +0000

    express added ready for deploy to heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/app'));
+
+app.listen(8080);
+  console.log("App listening on port 8080");
+
+app.get('*', function(req, res){
+  res.sendfile('../index.html');
+});
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
